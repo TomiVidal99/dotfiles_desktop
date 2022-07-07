6a7a8575d09a243381f808db224cf003393cdf4d
Tomas Vidal <tomi_vidal_99@hotmail.com>
2022-07-07 02:05:13 -0300

    Minor changes

--- .utility/i3/tray.sh
+++ .utility/i3/tray.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-pkill -f pasystray
-pkill -f blueman-tray
-pkill -f nm-applet
-pkill -f redshift-gtk
-
-pasystray --notify=all &
-blueman-tray &
-nm-applet --indicator &
-redshift-gtk &
-disown
+#sleep 15
+#
+#pkill -f pasystray
+#pkill -f blueman-tray
+#pkill -f nm-applet
+#pkill -f redshift-gtk
+#
+#pasystray --notify=all & disown
+#blueman-tray & disown
+#nm-applet --indicator & disown
+#redshift-gtk & disown
