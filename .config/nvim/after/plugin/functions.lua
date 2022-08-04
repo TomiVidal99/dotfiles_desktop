50d26d32b0e7a9a71f103ecff00afc765be007b4
Tomas Vidal <tomi_vidal_99@hotmail.com>
2022-08-04 14:02:53 -0300

    Added: custom function for add commit msg to current file

---
+++ .config/nvim/after/plugin/functions.lua
@@ -0,0 +1,12 @@
+-- Some custom functions.
+
+-- Git
+-- Add current file and prompt for a message to commit.
+function add_commit_current_file()
+  filepath=vim.fn.expand("%")
+  print(filepath)
+  os.execute("git add " .. filepath)
+  commit_message = vim.fn.input("Commit msg: ")
+  os.execute("git commit -m " .. "\"" .. commit_message .. "\"")
+end
+vim.keymap.set("n", "<leader>Gc", add_commit_current_file, {expr = true})
