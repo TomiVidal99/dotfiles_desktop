ce9406119a70097876a1cb7203d14d7700750088
Tomas Vidal <tomi_vidal_99@hotmail.com>
2022-08-15 16:21:24 -0300

    Added: new nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,7 @@
+-- TOMAS VIDAL's nvim config
+
+-- This file just loads some dependencies.
+require "tomii.options"
+require "tomii.keymaps"
+require "tomii.style"
+require "tomii.plugins"
---
+++ .config/nvim/lua/tomii/keymaps.lua
@@ -0,0 +1,136 @@
+-- Keymaps.
+
+-- Modes:
+--  normal_mode = "n",
+--  insert_mode = "i",
+--  visual_mode = "v",
+--  visual_block_mode = "x",
+--  term_mode = "t",
+--  command_mode = "c",
+
+-- Some helper variables
+local opts = {noremap = true, silent = true}
+local km = vim.api.nvim_set_keymap
+
+-- Helper function to set a normal keymap.
+kmn = function(key, func)
+  km("n", key, func, opts)
+end
+kmi = function(key, func)
+  km("i", key, func, opts)
+end
+kmv = function(key, func)
+  km("v", key, func, opts)
+end
+kmb = function(key, func)
+  km("x", key, func, opts)
+end
+
+-- Remap leader to space.
+km("", "<space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+---------- NORMAL ----------
+-- Save with root permission.
+vim.cmd "command! W w !sudo tee > /dev/null %"
+
+-- Delete without yank.
+vim.cmd 'nnoremap <leader>d "_d'
+vim.cmd 'nnoremap x "_x'
+
+-- Increment/decrement.
+vim.cmd "nnoremap + <C-a>"
+vim.cmd "nnoremap - <C-x>"
+
+-- Remap enter to ff (rapidly)
+vim.cmd "nmap ff <return>"
+
+-- Save file.
+kmn("<C-s>", "<CMD>w<CR>")
+
+-- Quickfixlist.
+kmn("<leader>qo", "<CMD>copen<CR>")
+kmn("<leader>qc", "<CMD>cclose<CR>")
+kmn("<leader>qn", "<CMD>cnext<CR>")
+kmn("<leader>qp", "<CMD>cprev<CR>")
+kmn("<leader>qh", "<CMD>chistory<CR>")
+
+-- Better window navigation.
+kmn("<C-h>", "<C-w>h")
+kmn("<C-j>", "<C-w>j")
+kmn("<C-k>", "<C-w>k")
+kmn("<C-l>", "<C-w>l")
+
+-- File explorer.
+kmn("<C-b>", "<CMD>Lexplore 30<CR>")
+
+-- Resize with alt+(h,j,k,l).
+kmn("<a-k>", "<CMD>resize -2<CR>")
+kmn("<a-j>", "<CMD>resize +2<CR>")
+kmn("<a-h>", "<CMD>vertical resize -2<CR>")
+kmn("<a-l>", "<CMD>vertical resize +2<CR>")
+
+-- Move between windows with s+(h,j,k,l)
+kmn("sh", "<CMD>wincmd h<CR>")
+kmn("sj", "<CMD>wincmd j<CR>")
+kmn("sk", "<CMD>wincmd k<CR>")
+kmn("sl", "<CMD>wincmd l<CR>")
+
+-- Move and split windows
+kmn("st", "<CMD>wincmd t<CR>")
+kmn("ss", "<CMD>wincmd s<CR>")
+kmn("sv", "<CMD>wincmd v<CR>")
+
+-- Navigate tabs.
+kmn("<C-l>", "<CMD>tabnext<CR>")
+kmn("<C-h>", "<CMD>tabprevious<CR>")
+
+-- Edit tabs.
+vim.cmd "nmap te :tabedit "
+kmn("tt", "<CMD>tabnew<CR>")
+
+-- Remove highlight
+vim.cmd "map <A-a> <CMD>noh<CR>"
+
+---------- INSERT ----------
+-- Quit insert.
+vim.cmd "imap <A-a> <Esc>"
+
+---------- VISUAL ----------
+-- Paste and don't yank.
+kmv("p", '"_dP')
+
+-- Quit visual mode with Alt+a.
+vim.cmd "vmap <A-a> <Esc>"
+
+-- Stay in indent mode.
+  kmv("<", "<gv")
+  kmv(">", ">gv")
+
+-- Move entire selected lines up and down.
+vim.cmd "vnoremap K :m '<-2<CR>gv=gv"
+vim.cmd "vnoremap J :m '>+1<CR>gv=gv"
+
+---------- VISUAL BLOCK ----------
+kmb("J", ":m '>+1<CR>gv-gv")
+kmb("K", ":m '<-2<CR>gv-gv")
+kmb("<A-j>", ":m '>+1<CR>gv-gv")
+kmb("<A-k>", ":m '<-2<CR>gv-gv")
+
+
+
+  -- TODO: maps to open things should go somewhere else
+--" Open a new terminal with the current path
+--map <F12> <CMD> exec '!konsole '.shellescape('%:p')' & disown' <CR>
+--tnoremap <A-a> <C-\><C-n> TODO: check this
+--
+--" Terminals inside nvim.
+--" start terminal in insert mode
+--au BufEnter * if &buftype == 'terminal' | :startinsert | endif
+--function! OpenTerminalBottom()
+--  split term://zsh
+--  resize 10
+--endfunction
+--nnoremap <leader>tb <CMD> call OpenTerminalBottom() <CR>
+--nnoremap <leader>tr <CMD> vsplit term://zsh <CR>
