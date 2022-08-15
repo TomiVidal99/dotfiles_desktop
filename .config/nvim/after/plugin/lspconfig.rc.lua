52e8f249d1c6c117fae0c4c0f349c79f43d92020
Tomas Vidal <tomi_vidal_99@hotmail.com>
2022-08-15 16:20:21 -0300

    Removed: old nvim config

--- .config/nvim/after/plugin/lspconfig.rc.lua
+++
@@ -1,4 +0,0 @@
--- more info: https://github.com/williamboman/mason-lspconfig.nvim
-
-local nvim_lsp = require "lspconfig"
-nvim_lsp.tailwindcss.setup {}
--- .config/nvim/after/plugin/nvim-ts-autotag.rc.lua
+++
@@ -1,7 +0,0 @@
--- more info: https://github.com/windwp/nvim-ts-autotag
-
-require'nvim-treesitter.configs'.setup {
-  autotag = {
-    enable = true,
-  }
-}
