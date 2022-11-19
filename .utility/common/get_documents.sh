b43db5a2b2cf33bc6567208859a2e08257260543
Tomás Vidal <tomi_vidal_99@hotmail.com>
2022-11-19 16:30:01 -0300

    changed find for fd and only display filenames in rofi

--- .utility/common/get_documents.sh
+++ .utility/common/get_documents.sh
@@ -18,20 +18,17 @@ function rofi_command() {
 }
 
 function get_documents() {
-  xargs -I '{}' find {} -type f -name "*.pdf"
+  xargs -I '{}' fd -p '.pdf' '{}'
 }
 
 function parse_documents() {
   xargs -I '{}' basename -s .pdf {}
 }
 
-DOCUMENTS_PATHS=$(cat $DIRECTORIES | get_documents)
 SELECTED_DOCUMENT=$(cat $DIRECTORIES | get_documents | parse_documents | rofi_command)
 SELECTED_DOCUMENT_PATH=$(cat $DIRECTORIES | get_documents | grep "$SELECTED_DOCUMENT")
 
-echo $SELECTED_DOCUMENT_PATH
-
-if [[ "$SELECTED_DOCUMENT_PATH" = "" || "$SELECTED_DOCUMENT" = "" ]]; then
+if [[ "$SELECTED_DOCUMENT" = "" ]]; then
   #rofi -e "You didn't select a document!\n"
   exit
 fi
