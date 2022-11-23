7888b020fc6487cb08668365e029a8b0e3172cbc
Tomás Vidal <tomi_vidal_99@hotmail.com>
2022-11-23 00:06:19 -0300

    Fixed everything, now it works flawlessly, just maybe optimize it a bit

--- .utility/common/get_documents.sh
+++ .utility/common/get_documents.sh
@@ -4,31 +4,40 @@
 # file and lets you (with rofi) open them in your file manager
 
 
-DOCUMENTS_READER=okular
 DIRECTORIES=~/.dotfiles/.utility/common/documents_dir
 
-# sets the folder icon by applying an echo pattern
-function apply_folder_pattern() {
-  xargs -I '{}' echo -en "{}\x00icon\x1fapplication-pdf\n"
-}
-
+# opens rofi with the right flags
 function rofi_command() {
-  apply_folder_pattern | rofi -dmenu -i -markup-rows -p "Seleccione un documento"
+  rofi -dmenu -i -markup -markup-rows -p "Seleccione un documento"
 }
 
-function parse_documents() {
-  xargs -I '{}' basename -s .pdf {}
+# returns the line parsed with the data
+function get_options_line() {
+  name=$(basename -s pdf "$1")
+  printf "<big><b>%s</b></big> - <small><i>%s</i></small>\x00icon\x1fapplication-pdf\n" "$name" "$1"
+}
+
+export -f get_options_line
+
+# feeds all the one line options to the line creator
+function parse_options() {
+  xargs -I '{}' bash -c 'get_options_line "{}"'
+}
+
+# extracts the path from the given return value
+function parse_selected_item() {
+  sed -e 's/<i>\(.*\)<\\i>/\1/' 
 }
 
 DOCUMENTS=$(cat $DIRECTORIES | xargs -I '{}' fd -e pdf . {})
-SELECTED_DOCUMENT_NAME=$(echo "$DOCUMENTS" | parse_documents | rofi_command)
-SELECTED_DOCUMENT_PATH=$(echo "$DOCUMENTS" | grep "$SELECTED_DOCUMENT_NAME")
+SELECTED_DOCUMENT_NAME=$(echo "$DOCUMENTS" | parse_options | rofi_command)
+SELECTED_DOCUMENT_PATH=$(expr "$SELECTED_DOCUMENT_NAME" | sed -E "s/.*<i>//" | sed -E "s/<\/i>.*//")
+
+echo "$SELECTED_DOCUMENT_NAME"
+echo "$SELECTED_DOCUMENT_PATH"
 
 if [[ "$SELECTED_DOCUMENT_NAME" = "" || "$SELECTED_DOCUMENT_PATH" = "" ]]; then
-  #rofi -e "You didn't select a document!\n"
   exit
 fi
 
-echo "Path: $SELECTED_DOCUMENT_PATH"
-
-$DOCUMENTS_READER "${SELECTED_DOCUMENT_PATH}" & disown
+xdg-open "$SELECTED_DOCUMENT_PATH"
