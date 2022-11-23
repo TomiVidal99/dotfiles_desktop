abc61393abd279a4d365c651d9f1c3354be1ef55
Tomás Vidal <tomi_vidal_99@hotmail.com>
2022-11-23 00:06:28 -0300

    Minor changes

--- .utility/i3/execute_rofi.sh
+++ .utility/i3/execute_rofi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rofi -show drun -modi "drun,run,filebrowser,emoji:/usr/bin/rofimoji" -show emoji -showicons -matching fuzzy
+rofi -show drun -modi "drun,run,filebrowser,emoji" -show emoji -showicons -matching fuzzy
