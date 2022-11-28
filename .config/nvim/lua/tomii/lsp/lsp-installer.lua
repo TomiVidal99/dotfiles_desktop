4d414bd8d5ff239197803ef285a3dc6047ddcc6a
Tomás Vidal <tomi_vidal_99@hotmail.com>
2022-11-28 13:25:57 -0300

    Changed hdl_checker for rust_hdl, install the lsp server with AUR package: 'rust_hdl-git'

--- .config/nvim/lua/tomii/lsp/lsp-installer.lua
+++ .config/nvim/lua/tomii/lsp/lsp-installer.lua
@@ -1,6 +1,6 @@
 local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 if not status_ok then
-	return
+  return
 end
 
 -- Import options from the other files
@@ -9,14 +9,14 @@ local capabilities = require("tomii.lsp.handlers").capabilities
 
 -- Register a handler that will be called for all installed servers.
 lsp_installer.setup({
-	automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
-	ui = {
-		icons = {
-			server_installed = "✓",
-			server_pending = "➜",
-			server_uninstalled = "✗",
-		},
-	},
+  automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+  ui = {
+    icons = {
+      server_installed = "✓",
+      server_pending = "➜",
+      server_uninstalled = "✗",
+    },
+  },
 })
 
 -- Define locallly the lsp
@@ -43,41 +43,49 @@ lsp.pyright.setup(lsps_opts)
 
 -- Server language for lua.
 lsp.sumneko_lua.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	settings = require("tomii.lsp.settings.sumneko_lua"),
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = require("tomii.lsp.settings.sumneko_lua"),
 })
 
 -- C#
 local pid = vim.fn.getpid()
 local omnisharp_executable = "omnisharp"
-local root_pattern = require('lspconfig.util').root_pattern
+local root_pattern = require("lspconfig.util").root_pattern
 lsp.omnisharp.setup({
-	-- TODO: move all this config to a different directory
-	capabilities = capabilities,
-	on_attach = on_attach,
-  root_dir = function (path)
-    return root_pattern('*.sln')(path) or root_pattern('*.csproj')(path)
+  -- TODO: move all this config to a different directory
+  capabilities = capabilities,
+  on_attach = on_attach,
+  root_dir = function(path)
+    return root_pattern("*.sln")(path) or root_pattern("*.csproj")(path)
   end,
-	cmd = { omnisharp_executable, "--languageserver", "--hostPID", tostring(pid) },
-	enable_editorconfig_support = true,
-	enable_ms_build_load_projects_on_demand = false,
-	enable_roslyn_analyzers = true,
-	organize_imports_on_format = true,
-	enable_import_completion = true,
-	sdk_include_prereleases = true,
-	analyze_open_documents_only = false,
+  cmd = { omnisharp_executable, "--languageserver", "--hostPID", tostring(pid) },
+  enable_editorconfig_support = true,
+  enable_ms_build_load_projects_on_demand = false,
+  enable_roslyn_analyzers = true,
+  organize_imports_on_format = true,
+  enable_import_completion = true,
+  sdk_include_prereleases = true,
+  analyze_open_documents_only = false,
 })
 
--- VHDL
-require("lspconfig").ghdl_ls.setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-	cmd = { "hdl_checker", "--lsp" },
-	filetypes = { "vhdl", "verilog", "systemverilog" },
-	root_dir = function(fname)
-		local util = require("lspconfig").util
-		return util.root_pattern(".hdl-prj.json")(fname) or util.path.dirname(fname)
-	end,
-	settings = {},
+local lspconfig = require("lspconfig")
+local configs = require("lspconfig.configs")
+
+-- Manual add rust_hdl server
+if not configs.rust_hdl then
+  configs.rust_hdl = {
+    default_config = {
+      cmd = { "vhdl_ls" },
+      filetypes = { "vhdl" },
+      root_dir = function(fname)
+        return lspconfig.util.root_pattern("vhdl_ls.toml")(fname) or vim.fn.getcwd()
+      end,
+      settings = {},
+    },
+  }
+end
+lsp.rust_hdl.setup({
+  on_attach = on_attach,
+  capabilities = capabilities
 })
