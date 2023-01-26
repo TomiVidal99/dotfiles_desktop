06b6494905ce51ec4e07a1cf01e25aaf55e06263
Tomás Vidal <tomi_vidal_99@hotmail.com>
2023-01-26 20:01:14 -0300

    Added auto format on save

--- .config/nvim/lua/tomii/lsp/lsp-installer.lua
+++ .config/nvim/lua/tomii/lsp/lsp-installer.lua
@@ -1,6 +1,9 @@
 local lsp_installer = require("nvim-lsp-installer")
 local lspconfig = require("lspconfig")
 
+-- auto format on save
+vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]])
+
 -- Register a handler that will be called for all installed servers.
 lsp_installer.setup({
 	automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
@@ -20,36 +23,36 @@ local lsps_opts = { on_attach = on_attach, capabilities = capabilities }
 
 -- LPSs
 local lsp_list = {
-  "clangd",
-  "tsserver",
-  "diagnosticls",
-  "tailwindcss",
-  "emmet_ls",
-  "cssls",
-  "pyright",
-  "jsonls"
+	"clangd",
+	"tsserver",
+	"diagnosticls",
+	"tailwindcss",
+	"emmet_ls",
+	"cssls",
+	"pyright",
+	"jsonls",
 }
 
 for _, lsp in pairs(lsp_list) do
-  lspconfig[lsp].setup(lsps_opts)
+	lspconfig[lsp].setup(lsps_opts)
 end
 
 -- TODO: think how to change the lsp_list to accept custom settings
 -- LUA
 lspconfig.sumneko_lua.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = require("tomii.lsp.settings.sumneko_lua"),
+	on_attach = on_attach,
+	capabilities = capabilities,
+	settings = require("tomii.lsp.settings.sumneko_lua"),
 })
 
 -- Custom LSPs
 local custom_lsps = {
-  "rust_hdl",
-  "bash_language_server",
-  "hdl_checker"
+	"rust_hdl",
+	"bash_language_server",
+	"hdl_checker",
 }
 for _, lsp in pairs(custom_lsps) do
-  lspconfig[lsp].setup(lsps_opts)
+	lspconfig[lsp].setup(lsps_opts)
 end
 
 -- mason-lspconfig allows me to automatically configure all installed LPSs
