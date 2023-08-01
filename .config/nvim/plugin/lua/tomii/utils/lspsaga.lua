a11d89b4e9c5f536ac383d0ba20cbb3917d1f6f2
Tomás Vidal <tomi_vidal_99@hotmail.com>
2023-08-01 19:51:57 -0300

    Merged nvim data from dotfiles_laptop

---
+++ .config/nvim/plugin/lua/tomii/utils/lspsaga.lua
@@ -0,0 +1,54 @@
+-- Lspsaga configuration
+local status_ok, lspsaga = pcall(require, "lspsaga")
+if not status_ok then
+	print("ERROR: lspsaga is not available. Called from lspsaga.lua")
+	return
+end
+
+lspsaga.setup({
+	ui = {
+		-- currently only round theme
+		theme = "round",
+		-- border type can be single,double,rounded,solid,shadow.
+		border = "rounded",
+		winblend = 0,
+		expand = "",
+		collapse = "",
+		preview = " ",
+		code_action = " ",
+		diagnostic = "🪲",
+		incoming = " ",
+		outgoing = " ",
+		colors = {
+			--float window normal background color
+			normal_bg = "#002b36",
+			--normal_bg = "#586e75",
+			--title background color
+			title_bg = "#255667",
+			red = "#dc322f",
+			magenta = "#d33682",
+			orange = "#cb4b16",
+			yellow = "#f7bb3b",
+			green = "#719e07",
+			cyan = "#2aa198",
+			blue = "#268bd2",
+			purple = "#6c71c4",
+			white = "#d1d4cf",
+			black = "#1c1c19",
+		},
+		kind = {},
+	},
+	hover = {
+		max_width = 0.6,
+		open_link = "gx",
+		open_browser = "!firefox",
+	},
+	symbol_in_winbar = {
+		enable = true,
+		separator = "    ",
+		hide_keyword = true,
+		show_file = true,
+		folder_level = 2,
+		respect_root = false,
+	},
+})
