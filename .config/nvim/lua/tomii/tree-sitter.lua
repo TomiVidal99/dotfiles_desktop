6f72c7cdf3a184f6caa0adaf414640d9f887a970
Tomás Vidal <tomi_vidal_99@hotmail.com>
2023-08-08 19:32:25 -0300

    Minor changes

--- .config/nvim/lua/tomii/tree-sitter.lua
+++ .config/nvim/lua/tomii/tree-sitter.lua
@@ -32,8 +32,19 @@ ts.setup({
 		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
 		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
 		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    colors = {
+      "#32a88d",
+      "#326fa8",
+      "#91104c",
+      "#914c10",
+    },
 	},
 	playground = {
 		enable = true,
 	},
 })
+
+vim.cmd[[hi rainbowcol1 guifg=#123456]]
+vim.cmd[[hi rainbowcol2 guifg=#326fa8]]
+vim.cmd[[hi rainbowcol3 guifg=#91104c]]
+vim.cmd[[hi rainbowcol4 guifg=#914c10]]
