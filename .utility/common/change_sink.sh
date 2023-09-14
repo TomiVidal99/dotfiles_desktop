5710fd5224297f17a3fc845619dc2d7a53d4fe73
Tomás Vidal <tomi_vidal_99@hotmail.com>
2023-09-14 09:07:00 -0300

    Now the change_sink.sh displays the names of the sinks

--- .utility/common/change_sink.sh
+++ .utility/common/change_sink.sh
@@ -1,22 +1,43 @@
 #!/bin/bash
 
-DMENU=${DMENU:-dmenu}
-INITIAL_MSG="Set new sink: "
+# Function to list available sinks in Rofi and return the selected sink index
+function select_sink() {
+  # Use pacmd to list sinks and their device.product.name
+  sink_info=$(pacmd list-sinks | awk '/index:/ {sinks[++i] = $0} /device\.product\.name/ {names[++k] = $0} {str[++w] sinks[i] ", " names[k]} END {for (j = 1; j <= i; j++) print sinks[j] names[j]}' | sed 's/\*/(selected)/')
 
-NEW_SINK="$( echo | ${DMENU} -fn "Inconsolata Condensed SemiBold 20" -nb "#e2845f" -sb "#000000" -p "$INITIAL_MSG" )"
+  # Extract the sink index and device.product.name for the Rofi menu
+  menu_text=""
+  while read -r sink_index device_product_name; do
+    menu_text="$menu_text$sink_index $device_product_name\n"
+  done <<< "$sink_info"
+  selected_sink_index=$(echo -e "$menu_text" | grep -v '^$' | rofi -dmenu -i -markup-rows -mesg "Select an audio sink" -p "Audio Sink" | awk -F': ' '/index:/ {sub(/[^0-9].*/, "", $2); print $2}')
+  echo "$selected_sink_index"
+}
 
-pactl set-default-sink $NEW_SINK
-RUNNING_SINK = "$(pactl short sinks | grep RUNNING | grep Name | awk '{print $2}')"
+# Let the user select the new audio sink
+NEW_SINK_INDEX=$(select_sink)
 
-pactl list short sink-inputs|while read stream; do
-    streamId=$(echo $stream|cut '-d ' -f1)
-    echo "moving stream $streamId"
-    pactl move-sink-input "$streamId" "$NEW_SINK"
+# Check if the user selected a sink
+if [ -z "$NEW_SINK_INDEX" ]; then
+    notify-send --hint=int:transient:1 -t 600 --urgency=critical "Error" "No sink selected"
+    exit 1
+fi
+
+# Set the selected sink as the default sink
+pacmd set-default-sink "$NEW_SINK_INDEX"
+
+# Get a list of all running audio sources
+sources=$(pacmd list-sink-inputs | grep 'index:' | awk '{print $2}')
+
+# Move the audio sources to the new active sink
+for source in $sources; do
+  pacmd move-sink-input "$source" "$NEW_SINK_INDEX"
 done
 
+# Notify the user of the change
 if [ $? == 0 ]; then
-    notify-send --hint=int:transient:1 -t 600 --urgency=critical "$RUNNING_SINK" "Default sink cambiada"
+    notify-send --hint=int:transient:1 -t 1000 --urgency=low "Default sink changed"
 else
-    notify-send --hint=int:transient:1 -t 600 --urgency=critical "ERROR" "No hay ninguna sink con ese numero"
+    notify-send --hint=int:transient:1 -t 1000 --urgency=low "Error" "Failed to change default sink"
 fi
 
