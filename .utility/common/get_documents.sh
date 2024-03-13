711f8dcadd0686059dba9a8ec2c993d3554a94fc
Tomás Vidal <tomi_vidal_99@hotmail.com>
2024-03-13 12:43:28 -0300

    Got get_documents working for all pdfs but now it's not well formatted, gotta check that out later

--- .utility/common/get_documents.sh
+++ .utility/common/get_documents.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+export LC_ALL=es_AR.UTF-8
+
 # Gets the documents listed in the 'documents_dir'
 # file and lets you (with rofi) open them in your file manager
 
 
-DIRECTORIES=~/.dotfiles/.utility/common/documents_dir
+DIRECTORIES=$HOME/.dotfiles/.utility/common/documents_dir
 
 # opens rofi with the right flags
 function rofi_command() {
@@ -12,9 +14,16 @@ function rofi_command() {
 }
 
 # returns the line parsed with the data
+#function get_options_line() {
+#  name=$(basename -s pdf "$1")
+#  printf "<big><b>%s</b></big> - <small><i>%s</i></small>\x00icon\x1fapplication-pdf\n" "$name" "$1"
+#}
 function get_options_line() {
-  name=$(basename -s pdf "$1")
-  printf "<big><b>%s</b></big> - <small><i>%s</i></small>\x00icon\x1fapplication-pdf\n" "$name" "$1"
+  local file_path="$1"
+  local file_name=$(basename "$file_path")
+  local file_dir=$(dirname "$file_path")
+  local name="${file_name%.*}"  # Remove the file extension
+  printf "<big><b>%s</b></big> - <small><i>%s</i></small>\x00icon\x1fapplication-pdf\n" "$name" "$file_dir"
 }
 
 export -f get_options_line
@@ -29,8 +38,11 @@ function parse_selected_item() {
   sed -e 's/<i>\(.*\)<\\i>/\1/' 
 }
 
-DOCUMENTS=$(cat $DIRECTORIES | xargs -I '{}' fd -e pdf . {})
-SELECTED_DOCUMENT_NAME=$(echo "$DOCUMENTS" | parse_options | rofi_command)
+find /path/to -regex ".*\.\(jpg\|gif\|png\|jpeg\)"
+#DOCUMENTS=$(cat $DIRECTORIES | xargs -I '{}' fd -e pdf . {})
+DOCUMENTS=$(cat $DIRECTORIES | xargs -I '{}' find {} -regex ".*\.\(pdf\)")
+#SELECTED_DOCUMENT_NAME=$(echo "$DOCUMENTS" | parse_options | rofi_command)
+SELECTED_DOCUMENT_NAME=$(echo "$DOCUMENTS" | rofi_command)
 SELECTED_DOCUMENT_PATH=$(expr "$SELECTED_DOCUMENT_NAME" | sed -E "s/.*<i>//" | sed -E "s/<\/i>.*//")
 
 echo "$SELECTED_DOCUMENT_NAME"
